d15ba14d75ce1848ac3b2de41798b5b6b4a49265
cerdikMY builder <madhanrao.v@gmail.com>
2026-06-17 07:56:10 +0000

    Add missing using Xunit; in integration test ApiFactory

--- tests/Cerdik.IntegrationTests/ApiFactory.cs
+++ tests/Cerdik.IntegrationTests/ApiFactory.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Xunit;
 
 namespace Cerdik.IntegrationTests;
 
