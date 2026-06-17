d46794e677c55603d448a94b02b9c310baa5fcf5
cerdikMY builder <madhanrao.v@gmail.com>
2026-06-17 07:57:46 +0000

    Add using Microsoft.AspNetCore.TestHost for ConfigureTestServices

--- tests/Cerdik.IntegrationTests/ApiFactory.cs
+++ tests/Cerdik.IntegrationTests/ApiFactory.cs
@@ -1,6 +1,7 @@
 using Cerdik.Infrastructure.Persistence;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
